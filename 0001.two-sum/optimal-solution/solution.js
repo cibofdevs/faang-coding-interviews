b64f8de2fce585be4e00829f428bf2fbea2eafaf
Ahmad Wijaya <cibofdev@gmail.com>
2023-07-22 03:25:21 +0700

    reformat code

--- 0001.two-sum/optimal-solution/solution.js
+++ 0001.two-sum/optimal-solution/solution.js
@@ -1,20 +1,19 @@
-const numsArray = [1,3,7,9,2];
+const numsArray = [1, 3, 7, 9, 2];
 const targetToFind = 11;
 
-const findTwoSum = function(nums, target) {
+const findTwoSum = function (nums, target) {
   const numsMap = {};
   
-  for(let p = 0; p < nums.length; p++) {
+  for (let p = 0; p < nums.length; p++) {
     const currentMapVal = numsMap[nums[p]];
-
-    if(currentMapVal >= 0) {
+    if (currentMapVal >= 0) {
       return [currentMapVal, p];
     } else {
       const numberToFind = target - nums[p];
       numsMap[numberToFind] = p;
     }
   }
-
+  
   return null;
 }
 
