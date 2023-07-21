371335b99b21155edc10307988ff5490b6ec369b
Ahmad Wijaya <cibofdev@gmail.com>
2023-07-22 03:17:56 +0700

    reformat code

--- 0001.two-sum/brute-force/solution.js
+++ 0001.two-sum/brute-force/solution.js
@@ -1,7 +1,7 @@
 const numsArray = [1, 3, 7, 9, 2];
 const targetToFind = 11;
 
-const findTwoSum = function(nums, target) {
+const findTwoSum = function (nums, target) {
   for (let p1 = 0; p1 < nums.length; p1++) {
     const numberToFind = target - nums[p1];
     for (let p2 = p1 + 1; p2 < nums.length; p2++) {
@@ -10,7 +10,7 @@ const findTwoSum = function(nums, target) {
       }
     }
   }
-
+  
   return null;
 };
 
