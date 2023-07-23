79c17e63d7e25b29fe555928ae7d89d1f271285a
Ahmad Wijaya <cibofdev@gmail.com>
2023-07-24 02:01:17 +0700

    reformat code

--- 03.trapping-rainwater/brute-force/solution.js
+++ 03.trapping-rainwater/brute-force/solution.js
@@ -1,28 +1,32 @@
 const elevationArray = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]
 
-const getTrappedRainwater = function(heights) {
+const getTrappedRainwater = function (heights) {
   let totalWater = 0;
   
-  for(let p = 0; p < heights.length; p++) {
+  for (let p = 0; p < heights.length; p++) {
+    console.log(`Looping ke-${p+1}`)
     let leftP = p, rightP = p, maxLeft = 0, maxRight = 0;
-
-    while(leftP >= 0) {
+    
+    while (leftP >= 0) {
       maxLeft = Math.max(maxLeft, heights[leftP]);
+      console.log({maxLeft});
       leftP--;
     }
-
-    while(rightP < heights.length) {
+    
+    while (rightP < heights.length) {
       maxRight = Math.max(maxRight, heights[rightP]);
+      console.log({maxRight});
       rightP++;
     }
     
     const currentWater = Math.min(maxLeft, maxRight) - heights[p];
-    
-    if(currentWater >= 0) {
+    console.log({currentWater})
+    if (currentWater >= 0) {
       totalWater += currentWater;
     }
+    console.log({totalWater})
   }
-
+  
   return totalWater;
 }
 
