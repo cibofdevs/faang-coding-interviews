f513e3e2173889768fdfc2b3aaf4c33e33cf33d2
Ahmad Wijaya <cibofdev@gmail.com>
2023-07-30 22:33:30 +0700

    add solution.js

---
+++ 05.longest-substring-without-repeating-characters/optimal/solution.js
@@ -0,0 +1,24 @@
+const string = "au"
+
+const lengthOfLongestSubstring = function (s) {
+    if (s.length <= 1) return s.length;
+
+    const seen = {};
+    let left = 0, longest = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        const currentChar = s[right];
+        const previouslySeenChar = seen[currentChar];
+
+        if (previouslySeenChar >= left) {
+            left = previouslySeenChar + 1;
+        }
+
+        seen[currentChar] = right;
+        longest = Math.max(longest, right - left + 1);
+    }
+
+    return longest;
+};
+
+console.log(lengthOfLongestSubstring(string));
